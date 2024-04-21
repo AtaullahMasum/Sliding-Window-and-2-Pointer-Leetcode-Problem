d379fd88c63f0aaf6848f42760e9c08ced8dc2aa
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-21 20:17:29 +0600

    Brute Force Solution Added

---
+++ 76. Minimum Window Substring.py
@@ -0,0 +1,25 @@
+# Brute Force Approch
+# Time Complexity O(n^2)
+# Space Complexity is O(256)
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        minLen, sIndex = 10**9, -1  
+        n, m = len(s), len(t)
+        for i in range(n):
+            hashMap = [0]*256
+            cnt = 0
+            for j in range(m):
+                hashMap[ord(t[j]) - 256] += 1
+            for j in range(i, n):  
+                if hashMap[ord(s[j]) - 256]> 0:
+                    cnt += 1
+                hashMap[ord(s[j]) - 256]  -= 1
+                if cnt == m:
+                    if (j - i + 1) < minLen:
+                        minLen = j - i + 1
+                        sIndex = i
+                        break
+        if sIndex == -1:
+            return ""
+        else:
+            return s[sIndex:sIndex+minLen]
