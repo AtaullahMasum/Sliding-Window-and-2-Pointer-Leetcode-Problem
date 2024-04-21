0647b866271d6ce36b7458508a0f89cd640d54ee
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-21 21:25:43 +0600

    optimal Solution added

--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -23,3 +23,33 @@ class Solution:
             return ""
         else:
             return s[sIndex:sIndex+minLen]
+# optimal Solution 
+# Time Complexity O(2n) + O(m)
+# Space Complexity is O(256)
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        minLen, sIndex = 10**9, -1  
+        n, m = len(s), len(t)
+        l, r, cnt = 0, 0, 0
+        hashMap = [0]*256
+        for i in range(m):
+            hashMap[ord(t[i])] += 1
+
+        while r < n:
+            if hashMap[ord(s[r])] > 0:
+                cnt += 1
+            hashMap[ord(s[r])] -= 1
+            while cnt == m:
+                if r - l + 1 < minLen:
+                    minLen = r - l + 1
+                    sIndex = l
+                hashMap[ord(s[l])] += 1
+                if hashMap[ord(s[l])] > 0:
+                    cnt -= 1
+                l += 1
+            r += 1
+        if sIndex == -1:
+            return ""
+        else:
+            return s[sIndex: sIndex+minLen]
+        
